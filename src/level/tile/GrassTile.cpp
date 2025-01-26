a54df94d043e4290ef802329041772b084688dfb
Team Omega <einfacheli.games@gmail.com>
2025-01-26 23:21:24 +0100

    let grass grow again

--- src/level/tile/GrassTile.cpp
+++ src/level/tile/GrassTile.cpp
@@ -12,22 +12,21 @@ int GrassTile::getTexture(int face)
 
 void GrassTile::tick(Level *level, int x, int y, int z, std::default_random_engine &random)
 {
-    if (!level->isLit(x, y, z))
-    {
-        level->setTile(x, y, z, Tile::dirt->id);
-    }
-    else
-    {
-        for (int i = 0; i < 4; ++i)
-        {
-            int nx = x + (random() % 3) - 1;
-            int ny = y + (random() % 5) - 3;
-            int nz = z + (random() % 3) - 1;
+    if(random() % 4 == 0){
+        if (!level->isLit(x, y + 1, z)){
+            level->setTile(x, y, z, Tile::dirt->id);
+        }
+        else{
+            for (int i = 0; i < 4; ++i){
+                int nx = x + (random() % 3) - 1;
+                int ny = y + (random() % 5) - 3;
+                int nz = z + (random() % 3) - 1;
 
-            if (level->getTile(nx, ny, nz) == Tile::dirt->id && level->isLit(nx, ny, nz))
-            {
-                level->setTile(nx, ny, nz, Tile::grass->id);
+                if (level->getTile(nx, ny, nz) == Tile::dirt->id && level->isLit(nx, ny + 1, nz)){
+                    level->setTile(nx, ny, nz, Tile::grass->id);
+                }
             }
         }
     }
+
 }
